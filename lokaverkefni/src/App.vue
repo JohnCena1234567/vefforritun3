a98c2006ee8a05de83382e478acc98295680336c
Artur <qerentin@gmail.com>
2017-11-27 10:08:31 +0000

    Lokaverkefni

---
+++ lokaverkefni/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data () {
+        return {
+            tasks: []
+        }
+    },
+
+    mounted() {
+        axios.get('http://fjolbraut.org/api/tasks', {
+            params: {
+                api_token: 'fXTKOL4rPrhKJanpYJqigSArVt0ImyM7stbaVHi0ToBaaOT2Iqpz3Aw0th8i'
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "~bulma/bulma";
+
+</style>
